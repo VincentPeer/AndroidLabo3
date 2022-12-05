d9fd406a14d8e5b856f2f1491b3c10db9cd10ac8
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-12-05 15:20:45 +0100

    add specific pict by note's type

--- app/src/main/java/ch/heigvd/daa_labo3/NotesListAdapter.kt
+++ app/src/main/java/ch/heigvd/daa_labo3/NotesListAdapter.kt
@@ -3,10 +3,12 @@ package ch.heigvd.daa_labo3
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ch.heigvd.daa_labo3.models.Note
 import ch.heigvd.daa_labo3.models.NoteAndSchedule
+import ch.heigvd.daa_labo3.models.Type
 
 class NotesListAdapter(_items : List<NoteAndSchedule> = listOf()) : RecyclerView.Adapter<NotesListAdapter.ViewHolder>() {
 
@@ -22,7 +24,7 @@ class NotesListAdapter(_items : List<NoteAndSchedule> = listOf()) : RecyclerView
 
 
     override fun getItemViewType(position: Int): Int {
-        return NOTE
+        return items[position].note.type.ordinal
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -38,14 +40,27 @@ class NotesListAdapter(_items : List<NoteAndSchedule> = listOf()) : RecyclerView
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val noteTitle = view.findViewById<TextView>(R.id.note_title)
         private val noteDescr = view.findViewById<TextView>(R.id.note_description)
+        private val noteTypePictogram = view.findViewById<ImageView>(R.id.note_type_pictogram)
+
         fun bind(note: NoteAndSchedule) {
             noteTitle.text = note.note.title
             noteDescr.text = note.note.text
+            noteTypePictogram.setImageResource(getDrawableId(note.note.type))
         }
+
     }
 
     companion object {
-        private val NOTE = 1
+
+        fun getDrawableId(noteType: Type): Int {
+            when (noteType) {
+                Type.NONE -> return R.drawable.note
+                Type.TODO -> return R.drawable.todo
+                Type.SHOPPING -> return R.drawable.shopping
+                Type.WORK -> return R.drawable.work
+                Type.FAMILY -> return R.drawable.family
+            }
+        }
     }
 
 }
--- app/src/main/java/ch/heigvd/daa_labo3/models/Type.kt
+++ app/src/main/java/ch/heigvd/daa_labo3/models/Type.kt
@@ -1,5 +1,5 @@
 package ch.heigvd.daa_labo3.models
 
-enum class Type {
+public enum class Type {
     NONE, TODO, SHOPPING, WORK, FAMILY
 }
